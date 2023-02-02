c4840f388db57a4ce9c429f25482fb3dc48fdfab
Bharat singh <116076581+bsakuni@users.noreply.github.com>
2023-02-02 21:34:56 +0530

    csv

---
+++ Boolean evaluation.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Simple expressions
+
+# Boolean Evaluation
+
+# In[1]:
+
+
+True and False
+
+
+# In[2]:
+
+
+True and True
+
+
+# In[3]:
+
+
+True or False
+
+
+# In[4]:
+
+
+not True
+
+
+# In[5]:
+
+
+not False
+
+
+# In[6]:
+
+
+True is True
+
+
+# In[7]:
+
+
+True is False
+
+
+# In[8]:
+
+
+'a' is 'a'
+
+
+# In[9]:
+
+
+True + True
+
+
+# In[10]:
+
+
+True + False
+
+
+# In[11]:
+
+
+False + False
+
+
+# In[12]:
+
+
+True - False
+
+
+# In[13]:
+
+
+False  - False
+
+
+# In[14]:
+
+
+False - True
+
+
+# In[15]:
+
+
+10 == 10
+
+
+# In[16]:
+
+
+10 <= 10
+
+
+# In[17]:
+
+
+True and False or True
+
